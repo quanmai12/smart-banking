495c8f52210fae469368cdb8ce8aa65350e1f13e
quanmai12 <maiquanxt4@gmail.com>
2025-03-04 16:35:06 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+
+
+  return (
+    <Router>
+        <Routes>
+          {/* <Route path="/" element={} /> */}
+        </Routes>
+    </Router>
+  )
+}
+
+export default App
