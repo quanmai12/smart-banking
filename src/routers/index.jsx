edc559278578d511f7773b8aeaf36ce6589944c0
quanmai12 <maiquanxt4@gmail.com>
2025-03-10 17:39:05 +0700

    resolved conflict

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import {Signin , Signup , EmailConfirm} from "../pages/login";
-
+import HomeScreen from "../pages/home_screen/";
 
 const AppRoutes = () => {
   return (
@@ -12,7 +12,7 @@ const AppRoutes = () => {
           <Route path="dashboard" />
           <Route path="login" element={<Signin />} />
           <Route path="signup" element={<Signup />} />
-          <Route path="/homescreen" element={<HomeScreen />}
+          <Route path="/homescreen" element={<HomeScreen />} />
           <Route path="email-confirm" element={<EmailConfirm />} />
         </Route>
       </Routes>
